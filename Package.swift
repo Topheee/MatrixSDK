a5c44727cd862e9aaf1cef5f669225d1f12d1f8f
Christopher Kobusch <ck@peeree.de>
2021-06-04 01:30:38 +0200

    change to allow for rebuild

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "MatrixSDK", targets: ["MatrixSDK"])
     ],
     targets: [
-        .binaryTarget(name: "MatrixSDK", url: "https://github.com/Topheee/MatrixSDK/releases/download/v0.19.0/MatrixSDK.xcframework.zip", checksum: "5262b6bd98f44438e7f72a2346dcdf3a3af021f0eaa0357b727242e696c6b819")
+        .binaryTarget(name: "MatrixSDK", url: "https://github.com/Topheee/MatrixSDK/releases/download/v0.19.0/MatrixSDK.xcframework.zip", checksum: "changetoallowforrebuild")
     ]
 )
