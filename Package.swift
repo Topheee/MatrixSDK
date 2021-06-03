501b8c8ed7d1c1cde834bf71a55778096a2be024
Christopher Kobusch <ck@peeree.de>
2021-06-03 16:19:35 +0200

    v0.19.0 iOS compat

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "MatrixSDK", targets: ["MatrixSDK"])
     ],
     targets: [
-        .binaryTarget(name: "MatrixSDK", url: "https://github.com/niochat/MatrixSDK/releases/download/v0.18.4/MatrixSDK.xcframework.zip", checksum: "368a357321b75d390490cf8925c423f7861461ca383609c690137305cfe04860")
+        .binaryTarget(name: "MatrixSDK", url: "https://github.com/niochat/MatrixSDK/releases/download/v0.19.0/MatrixSDK.xcframework.zip", checksum: "368a357321b75d390490cf8925c423f7861461ca383609c690137305cfe04860")
     ]
 )
