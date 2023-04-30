09aa9edff5e9c24ec1ec944c225999fb147e7a73
Christopher Kobusch <ck@peeree.de>
2023-04-30 10:12:14 +0200

    build v0.26.15

--- generate-package.swift
+++ generate-package.swift
@@ -14,8 +14,7 @@ func packageString(url: String, checksum: String) -> String {
         dependencies: [
             .package(url: "https://github.com/realm/realm-swift", exact: "10.27.0"),
             .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", exact: "1.9.5"),
-            .package(url: "https://gitlab.matrix.org/matrix-org/olm", from: "3.2.5"),
-            .package(url: "https://github.com/Topheee/MatrixSDKCrypto", from: "1.1.0")
+            .package(url: "https://gitlab.matrix.org/matrix-org/olm", from: "3.2.5")
         ],
         targets: [
             .binaryTarget(name: "MatrixSDK", url: "\(url)", checksum: "\(checksum)")
